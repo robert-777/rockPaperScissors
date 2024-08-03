4941b586fbef86b40c8a062a80ade8713ff458f1
robert-777 <64499850+robert-777@users.noreply.github.com>
2024-08-03 19:43:35 +0200

    Write the logic for human choice

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ function getComputerChoice() {
     return choices[randomChoice];
 }
 
+function getHumanChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    let humanChoice = prompt("What is our choice? (options: rock, paper, scissors)");
+    while (!choices.includes(humanChoice)) {
+        humanChoice = prompt("Invalid choice. Please choose between rock, paper, scissors");
+    }
+    return humanChoice;
+}
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
+
+console.log(getHumanChoice());
